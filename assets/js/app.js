ab9e70723cebb8b24ed1a1a5c38dd636af0bc5f4
Fred Mohier <frederic/mohier@gmail.com>
2019-06-22 10:08:20 +0200

    Bootstrap with Encore

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,11 +7,13 @@ const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
+require("bootstrap/dist/css/bootstrap.css");
 
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
 $(document).ready(function() {
+    console.log("Ready!");
     $('[data-toggle="popover"]').popover();
 });
